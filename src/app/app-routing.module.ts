07ec0f825f8c05e0bec53548b9ac1c45b8d8c285
Mouhammad Zaher abbas <z.abbas83@yahoo.com>
2023-06-21 14:59:36 +0200

    Guard logic!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { RdvlistComponent } from './rdvlist/rdvlist.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
-   {path: '', component: CalendarComponent },
-   {path: 'calendar', component: CalendarComponent }, //by default path
+   {path: '', component: CalendarComponent, canActivate: [authGuard] },
+   {path: 'calendar', component: CalendarComponent, canActivate: [authGuard] }, //by default path
    {path: 'login', component: LoginComponent },
    {path: 'register', component: RegisterComponent }
 ];
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,9 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from './services/user.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  if (!inject(UserService).isLoggedIn)
+  inject(Router).navigate(['/login']);
+   return inject(UserService).isLoggedIn
 };
